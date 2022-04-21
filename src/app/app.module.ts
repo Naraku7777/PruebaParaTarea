30d84201cd86056b0ed34a3d7f7b04b41ee9941e
Naraku7777 <vargasdaniel195@gmail.com>
2022-04-21 17:05:46 -0600

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MatSliderModule } from '@angular/material/slider';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 
 import { AppComponent } from './inicio/app.component';
 
-
-
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatToolbarModule, MatIconModule, MatMenuModule],
+  imports:      [ BrowserModule, FormsModule, MatToolbarModule, MatIconModule, MatMenuModule, MatSliderModule],
   declarations: [AppComponent],
   bootstrap:    [ AppComponent ]
 })

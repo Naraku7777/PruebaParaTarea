99f4631d8807647cd75a4f9a6d321668536068c5
Naraku7777 <vargasdaniel195@gmail.com>
2022-05-01 17:46:06 -0600

    5 files modified, 21 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { MatSliderModule } from '@angular/material/slider';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-import { AppComponent } from './inicio/app.component';
+import { AppMaterialModule } from './shared/app-material.module';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppRoutingModule } from './app-routing.module';
+
+import { Page1Component } from './Pages/page1/page1.component';
+import { Page2Component } from './Pages/page2/page2.component';
+import { Page3Component } from './Pages/page3/page3.component';
+import { Page4Component } from './Pages/page4/page4.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatToolbarModule, MatIconModule, MatMenuModule, MatSliderModule],
-  declarations: [AppComponent],
+  imports:      [ BrowserModule, FormsModule, AppMaterialModule, FlexLayoutModule, AppRoutingModule ],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent, Page1Component, Page2Component, Page3Component, Page4Component ],
   bootstrap:    [ AppComponent ]
 })
-
 export class AppModule { }
+
